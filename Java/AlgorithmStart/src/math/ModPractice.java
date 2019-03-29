c99c46f7bde694c631767c8c254a405320d23bcd
jieun <jieun3001@gmail.com>
2019-03-29 12:21:26 +0900

    * Mod Practice

---
+++ Java/AlgorithmStart/src/math/ModPractice.java
@@ -0,0 +1,27 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ModPractice {
+	/*
+	 * (A+B)%C=(A%C+B%C)% C
+	 * (A*B)%C = (A%C * B%C)%C 
+	 * °ö¼À°ú µ¡¼ÀÀº °á°ú°¡ °°´Ù
+	 */
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		System.out.println((A + B) % C);
+		System.out.println((A % C) + (B % C) % C);
+		System.out.println((A * B) % C);
+		System.out.println((A % C) * (B % C) % C);
+
+	}
+
+}
